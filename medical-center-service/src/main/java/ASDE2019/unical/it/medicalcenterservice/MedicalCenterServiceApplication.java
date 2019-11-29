d00c3f1f20f458efc91a82036e83ec7c42e87e28
ArmRandoM <armnd.6793@gmail.com>
2019-11-29 13:14:43 +0100

    First Initialization

---
+++ medical-center-service/src/main/java/ASDE2019/unical/it/medicalcenterservice/MedicalCenterServiceApplication.java
@@ -0,0 +1,13 @@
+package ASDE2019.unical.it.medicalcenterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCenterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCenterServiceApplication.class, args);
+	}
+
+}
---
+++ medical-center-service/src/test/java/ASDE2019/unical/it/medicalcenterservice/MedicalCenterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ASDE2019.unical.it.medicalcenterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCenterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
