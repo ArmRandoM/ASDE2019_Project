54e5c1d8dddad6af97d2548b95322b4d91d93217
ArmRandoM <armnd.6793@gmail.com>
2020-01-05 17:41:23 +0100

    Changes by Armando: Menu Bar updated

--- medical-center-webapp/src/components/MenuBar.js
+++ medical-center-webapp/src/components/MenuBar.js
@@ -1,38 +1,20 @@
 import React from 'react';
-import {fade,makeStyles} from '@material-ui/core/styles';
-import {Button, Grid} from '@material-ui/core';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import ListItem from '@material-ui/core/ListItem';
-import ChatIcon from '@material-ui/icons/Chat';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import HomeIcon from '@material-ui/icons/Home';
-import SearchIcon from '@material-ui/icons/Search';
+import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import PostAddIcon from '@material-ui/icons/PostAdd';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const useStyles = makeStyles(theme => ({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
-  root: {
+  grow: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1,
     display: 'none',
     [theme.breakpoints.up('sm')]: {
       display: 'block',
@@ -45,10 +27,11 @@ const useStyles = makeStyles(theme => ({
     '&:hover': {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
+    marginRight: theme.spacing(2),
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
+      marginLeft: theme.spacing(3),
       width: 'auto',
     },
   },
@@ -68,108 +51,78 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(1, 1, 1, 7),
     transition: theme.transitions.create('width'),
     width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 120,
-      '&:focus': {
-        width: 200,
-      },
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
     },
   },
 }));
 
-export default function SwipeableTemporaryDrawer(props) {
+export default function PrimarySearchAppBar() {
   const classes = useStyles();
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-
-  const toggleDrawer = (side, open) => event => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState({ ...state, [side]: open });
-  };
-
-  const sideList = side => (
-    <div
-      className={classes.list}
-      role="presentation"
-      onClick={toggleDrawer(side, false)}
-      onKeyDown={toggleDrawer(side, false)}
-    >
-      
-        <IconButton onClick={toggleDrawer('left', true)}>
-            <ChevronLeftIcon />
-        </IconButton>
-        <List>
-          <ListItem>
-            <Button href="/homepg"><HomeIcon/>HomePage</Button>
-          </ListItem>
-        </List>
-        <Divider />
-        <Divider />
-        <List>
-          <ListItem>
-            <Button href="/reportpg"><LocalHospitalIcon/>My Report</Button>
-          </ListItem>
-          <ListItem>
-            <Button href="/chatpg"><ChatIcon/>Chats</Button>
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-          <ListItem>
-            <Button href="/profilepg"><AccountCircleIcon/>My Profile</Button>
-          </ListItem>
-          <ListItem>
-            <Button href="/" onClick={() => props.logOut()}><ExitToAppIcon/>LogOut</Button>
-          </ListItem>
-        </List>
-
-    </div>
-  );
 
   return (
-    <div>
-
-      <AppBar position="fixed">
-        <Toolbar variant="dense">
-          <Grid container>
-            <Grid item xs={4} sm={4} md={2}>
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer('left', true)}>
-                <MenuIcon /> Menu
-              </IconButton>
-            </Grid>
-            <Grid item xs={2} sm={2} md={8}></Grid>
-            <Grid item xs={6} sm={6} md={2}>
-              <div className={classes.search}>
-                <div className={classes.searchIcon}>
-                  <SearchIcon />
-                </div>
-                <InputBase
-                  placeholder="Search…"
-                  classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                  }}
-                  inputProps={{ 'aria-label': 'search' }}
-                />
-              </div>
-            </Grid>
-          </Grid>
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            MEDIC-AI
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton
+              title="Profile"
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+              color="inherit"
+              href="profilepg"
+            >
+              <AccountCircle />
+            </IconButton>
+            <IconButton
+              title="Add Report"
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+              color="inherit"
+              href="reportpg"
+            >
+              <PostAddIcon />
+            </IconButton>
+            <IconButton
+              title="Log-Out"
+              color="inherit">
+              <ExitToAppIcon />
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
-      <SwipeableDrawer
-        open={state.left}
-        onClose={toggleDrawer('left', false)}
-        onOpen={toggleDrawer('left', true)}
-      >
-        {sideList('left')}
-      </SwipeableDrawer>
     </div>
   );
 }
